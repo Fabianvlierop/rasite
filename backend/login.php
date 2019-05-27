33ac466e6d577d495fa66e6a39226ddf98036d81
Fabianvlierop <45451428+Fabianvlierop@users.noreply.github.com>
2019-05-27 14:33:29 +0200

    Issue 1 opgelost

--- backend/login.php
+++ backend/login.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'dbconfig.php';
-session_start();  
+session_start();
 
 if(!empty($_SESSION['username'])){
   header("Location: admin.php");
@@ -16,6 +16,7 @@ if(isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == "POST"){
     $row = $result->fetch_assoc();
     if(password_verify($password, $row['password'])){
   		$_SESSION['username'] = $username;
+      $_SESSION['user_rights'] = $row['user_rights'];
   		header('Location: admin.php');
   	}else{
   		$error = "Username or password incorrect.";
